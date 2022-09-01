855a7e5266954d51888ce619fc93473fd94cd5a2
Guilherme Knorst Magnago <knorstmagnago@gmail.com>
2022-09-01 12:13:34 +0200

    read_high_score function
    Defined read_high_score(self) and added it to instances where high_score should be retrieved from the txt file

--- score.py
+++ score.py
@@ -6,8 +6,7 @@ class Score (Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        with open("high_score.txt") as hs:
-            self.high_score = int(hs.read())
+        self.read_high_score()
         self.speed("fastest")
         self.penup()
         self.color("white")
@@ -27,11 +26,13 @@ class Score (Turtle):
         if self.score > int(self.high_score):
             with open("high_score.txt", mode="w") as hs:
                 hs.write(str(self.score))
-        with open("high_score.txt") as hs:
-            self.high_score = int(hs.read())
+        self.read_high_score()
         self.score = 0
         self.update()
 
+    def read_high_score(self):
+        with open("high_score.txt") as hs:
+            self.high_score = int(hs.read())
     # def game_over(self):
     #     self.goto(0, 0)
     #     self.write(f"GAME OVER", False, "center", ("Courier", 16, "normal"))
